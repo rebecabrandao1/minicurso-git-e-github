0f37b0067e812dc050c05714e33374c81eb231de
leandrowqtc <devleandromcz@gmail.com>
2023-11-06 10:23:55 -0300

    Aplicação Java

--- aplicacao.java
+++ aplicacao.java
@@ -1,5 +1,12 @@
 public class aplicacao {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "João";
+        pessoa.idade = 20;
+        pessoa.sexo = "Masculino";
+        pessoa.cpf = "123.456.789-00";
+        System.out.println("Hello World! Me chamo "+pessoa.nome+" e tenho "+pessoa.idade+" anos.");
+        System.out.println("Meu sexo é "+pessoa.sexo+" e meu CPF é "+pessoa.cpf+".");
+
     }
 }
