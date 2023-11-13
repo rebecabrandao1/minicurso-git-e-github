ebffc30d893df46c5d013f019d783c2c5e6e702a
Leandro <devleandromcz@gmail.com>
2023-11-13 01:05:27 -0300

    Aplicação e documentação

--- Pessoa.java
+++
@@ -1,6 +0,0 @@
-public class Pessoa {
-    String nome;
-    int idade;
-    String sexo;
-    String cpf;
-}
--- aplicacao.java
+++ aplicacao.java
@@ -1,12 +1,49 @@
+import java.util.Scanner;
+
 public class aplicacao {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-        pessoa.nome = "João";
-        pessoa.idade = 20;
-        pessoa.sexo = "Masculino";
-        pessoa.cpf = "123.456.789-00";
-        System.out.println("Hello World! Me chamo "+pessoa.nome+" e tenho "+pessoa.idade+" anos.");
-        System.out.println("Meu sexo é "+pessoa.sexo+" e meu CPF é "+pessoa.cpf+".");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bem vindo a calculadora");
 
+        while (true) {
+            System.out.println("Digite o primeiro numero");
+            float num1 = input.nextFloat();
+            System.out.println("Digite o segundo numero");
+            float num2 = input.nextFloat();
+            float resultado = 0;
+            String operacao;
+            while (true) {
+                System.out.println("Digite a operacao");
+                operacao = input.next();
+                if (operacao.equals("+")) {
+                    resultado = num1 + num2;
+                    break;
+                } else if (operacao.equals("-")) {
+                    resultado = num1 - num2;
+                    break;
+                } else if (operacao.equals("*")) {
+                    resultado = num1 * num2;
+                    break;
+                } else if (operacao.equals("/")) {
+                    resultado = num1 / num2;
+                    break;
+                } 
+                else if(operacao.equals("sair")){
+                    break;
+                }else {
+                    System.out.println("Operacao invalida. Tente novamente, ou digite 'sair' para sair");
+                }
+            }
+            
+            if(!operacao.equals("sair")){
+                System.out.println("O resultado da operacao e: " + resultado);
+            }
+            System.out.println("Deseja continuar? (s/n)");
+            String continuar = input.next();
+            if (continuar.equals("n")) {
+                System.out.println("Obrigado por usar a calculadora");
+                break;
+            }
+        }
     }
 }
